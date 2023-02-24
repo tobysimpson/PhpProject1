8657d52460a717ef29af101d5a6e67cf263de02b
Toby Simpson <toby.simpson@usi.ch>
2023-02-24 15:12:35 +0100

    put user stuff in db class

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require "db1.php";
+$db = new db1();
+?>
 <html>
     <head>
         <title></title>
@@ -5,11 +9,20 @@
         <script src="xmlhttp.js"></script>
         <script type="text/javascript">
 
-        //hello
+            function fn_init()
+            {
+                div1.innerText = "hello";
+            }
+
         </script>
     </head>
-    <body>
+    <body onload="fn_init();">
 
+        <div><?php echo "usr_id=", $db->$usr_id;?></div>
+        
+        <a href="usr.php">usr_info</a>
+        <a href="itm.php">item_info</a>
+        
         <div id="frm1">
             <input id="item_id" type="text"   value="1">
             <input id="btn1" type="button" value="submit" onclick="fn_submit(frm1,'/xml.php',div1);" >
